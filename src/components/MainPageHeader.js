683827ce4533a2409970fa915ea99112c1ad0d8e
Major <matmaj81@wp.pl>
2020-01-11 17:30:24 +0100

    logo added

--- src/components/MainPageHeader.js
+++ src/components/MainPageHeader.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 export default function MainPageHeader() {
   return (
     <Container>
-      
+        <img src = "../graphics/rysunek4"></img>
     </Container>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Container from "react-bootstrap/Container";
+import MainPageHeader from "../components/MainPageHeader";
 
 function Index() {
   return (
     <Container>
       <h1>Siema siema kurwa witam twoja stara to kapitan!</h1>
+      <MainPageHeader/>
     </Container>
   );
 }
