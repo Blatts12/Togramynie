f2f023a4729b852c1bf5133cc42850369245b8cb
Major <matmaj81@wp.pl>
2020-01-29 10:50:17 +0100

    gameModel

---
+++ src/models/gameModel.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var gameStats = new Schema({
+    value_name: String,
+    value_max: Number,
+    value_default: Number
+});
+
+var inputComponent = new Schema({
+    value: Number
+});
+
+var gameStatsModel = mongoose.model("GameStatsModel", gameStats);
+var inputComponentModel = mongoose.model("Input", inputComponent);
+
+module.exports = mongoose.model("TestGame",{
+    game_name: String,
+    game_max_players: Number,
+    game_stats: [gameStats],
+    incrementators: [inputComponent],
+    decrementators: [inputComponent]
+});
+
+module.exports.CreateInputComponent = (value) => {
+    var input = new inputComponentModel();
+    input.value = value;
+
+    return input;
+};
